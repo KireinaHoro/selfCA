2341f4dd103572d6b6f6b99c59d82239e6bae540
KireinaHoro <i@jsteward.moe>
2017-11-09 18:28:34 +0800

    check in configuration for ease of config

---
+++ config.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# selfCA config
+# All the configs are in the form of `KEY=VALUE` as bash variables.
+
+# Root dir to store all your certs and keys. Should in a safe and sound place.
+# It don't need to be exist; the script will create it.
+SELFCA_ROOT=/home/jsteward/ca
+
+# Root certificate effective days
+ROOT_CERT_DAYS=7300
+
+# Root key length
+ROOT_KEY_LENGTH=4096
+
+# Intermediate cert name
+INTERMEDIATE_CERT_NAME=server-intermediate
+
+# Intermediate certificate effective days
+INTERMEDIATE_CERT_DAYS=3650
+
+# Intermediate key length
+INTERMEDIATE_KEY_LENGTH=4096
+
+# Website cert key length
+DEFAULT_KEY_LENGTH=2048
+
+# Certificate distribute folder
+DISTRIBUTE_DIR=/tmp/ca-dist
